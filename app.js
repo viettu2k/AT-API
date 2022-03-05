82d1a5e9abc2816d19695b9970fa54ece4c25fe0
viettu2k <doanviettu2k@gmail.com>
2022-03-05 14:53:46 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+    res.send("hello from node");
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
