b693444a0e918624daea927dc9486ea19eb95534
viettu2k <doanviettu2k@gmail.com>
2022-03-12 10:21:07 +0700

    fix, and adding signout

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+
+// import routes
+const userRoutes = require("./routes/user");
+
+// app
+const app = express();
+
+// connect db
+mongoose.connect(process.env.DATABASE).then(() => console.log("DB connected"));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(cookieParser());
+app.use(expressValidator());
+
+// routes middleware
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- models/user.js
+++ models/user.js
@@ -1,12 +1,64 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require("uuid");
 
 const userSchema = new mongoose.Schema({
-    mail: {
+    name: {
         type: String,
+        trim: true,
+        require: true,
+        maxLength: 32,
+    },
+    email: {
+        type: String,
+        trim: true,
+        require: true,
         unique: true,
     },
-    username: { type: String },
-    password: { type: String },
-});
+    hash_password: {
+        type: String,
+        require: true,
+    },
+    about: {
+        type: String,
+        trim: true,
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0,
+    },
+    history: {
+        type: Array,
+        default: [],
+    },
+}, { timestamps: true });
 
-module.exports = mongoose.model("user", userSchema);
+// virtual field
+userSchema
+    .virtual("password")
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hash_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hash_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            return crypto.createHmac("sha1", this.salt).update(password).digest(hex);
+        } catch (err) {
+            return "";
+        }
+    },
+};
+
+module.exports = mongoose.model("User", userSchema);
