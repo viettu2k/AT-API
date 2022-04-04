6d7426b58df178dcf7b9a86d14a61a21069dcf96
viettu2k <doanviettu2k@gmail.com>
2022-04-04 10:23:13 +0700

    adding list class room by user

--- controllers/classRoom.js
+++ controllers/classRoom.js
@@ -3,26 +3,38 @@ const { errorHandler } = require("../helpers/dbErrorHandler");
 const _ = require("lodash");
 
 exports.create = (req, res) => {
-  const classRoom = new ClassRoom({ ...req.body, createdBy: req.params });
-  classRoom.save((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err),
-      });
-    }
-    res.json({ message: "Created successfully", data });
-  });
+    const classRoom = new ClassRoom({...req.body, createdBy: req.params });
+    classRoom.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err),
+            });
+        }
+        res.json({ message: "Created successfully", data });
+    });
 };
 
 exports.update = (req, res) => {
-  let classRoom = req.classRoom;
-  classRoom = _.extend(classRoom, req.body);
-  classRoom.save((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err),
-      });
-    }
-    res.json(data);
-  });
+    let classRoom = req.classRoom;
+    classRoom = _.extend(classRoom, req.body);
+    classRoom.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err),
+            });
+        }
+        res.json(data);
+    });
+};
+
+exports.listByUser = (req, res) => {
+    ClassRoom.find({ createdBy: req.profile._id })
+        .populate("createdBy", "_id")
+        .sort("-createdAt")
+        .exec((err, classRooms) => {
+            if (err) {
+                return res.status(400).json({ error: err });
+            }
+            res.json(classRooms);
+        });
 };
