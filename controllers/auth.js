6fc9d5aa6218c2950d291756522117d38a654907
viettu2k <doanviettu2k@gmail.com>
2022-04-09 22:53:27 +0700

    refactoring

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,136 +5,136 @@ const expressJwt = require("express-jwt");
 const _ = require("lodash");
 
 exports.signup = (req, res) => {
-    const user = new User(req.body);
-    user.save((err, user) => {
-        if (err) {
-            return res.status(400).json({
-                error: "Email is taken",
-            });
-        }
-        user.salt = undefined;
-        user.hashed_password = undefined;
-        res.json({
-            user,
-        });
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+    res.json({
+      user,
     });
+  });
 };
 
 exports.signin = (req, res) => {
-    // find the user based on email
-    const { email, password } = req.body;
-    User.findOne({ email }, (error, user) => {
-        if (error || !user) {
-            return res.status(400).json({
-                error: "User with that email does not exist. Please signup",
-            });
-        }
-
-        // if user is found make sure the email and password match
-        // create authenticate method in user model
-        if (!user.authenticate(password)) {
-            return res.status(401).json({
-                error: "Email and password dont match",
-            });
-        }
-        // generate a signed token with user id and secret
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-        // persist the token as "t" in cookie with expiry date
-        res.cookie("t", token, { expire: new Date() + 9999 });
-        // return response with user and token to frontend client
-        const { _id, name, email, role } = user;
-        return res.json({ token, user: { _id, email, name, role } });
-    });
+  // find the user based on email
+  const { email, password } = req.body;
+  User.findOne({ email }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please signup",
+      });
+    }
+    // if user is found make sure the email and password match
+    // create authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password dont match",
+      });
+    }
+    // generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    // persist the token as "t" in cookie with expiry date
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    // return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
 };
 
 exports.requireSignin = expressJwt({
-    secret: process.env.JWT_SECRET,
-    algorithms: ["HS256"],
-    userProperty: "auth",
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+  userProperty: "auth",
 });
 
 exports.isAuth = (req, res, next) => {
-    let user = req.profile && req.auth && req.profile._id == req.auth._id;
-    if (!user) {
-        return res.status(403).json({
-            error: "Access denied!",
-        });
-    }
-    next();
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!user) {
+    return res.status(403).json({
+      error: "Access denied!",
+    });
+  }
+  next();
 };
 
 exports.forgotPassword = (req, res) => {
-    if (!req.body) return res.status(400).json({ message: "No request body" });
-    if (!req.body.email) {
-        return res.status(400).json({ message: "No Email in request body" });
-    }
-    // console.log("forgot password finding user with that email");
-    const { email } = req.body;
-    // find the user based on email
-    User.findOne({ email }, (err, user) => {
-        // if err or no user
-        if (err || !user)
-            return res.status("401").json({
-                error: "User with that email does not exist!",
-            });
+  if (!req.body) return res.status(400).json({ message: "No request body" });
+  if (!req.body.email) {
+    return res.status(400).json({ message: "No Email in request body" });
+  }
+  // console.log("forgot password finding user with that email");
+  const { email } = req.body;
+  // find the user based on email
+  User.findOne({ email }, (err, user) => {
+    // if err or no user
+    if (err || !user)
+      return res.status("401").json({
+        error: "User with that email does not exist!",
+      });
 
-        // generate a token with user id and secret
-        const token = jwt.sign({ _id: user._id, iss: "EMH" },
-            process.env.JWT_SECRET
-        );
+    // generate a token with user id and secret
+    const token = jwt.sign(
+      { _id: user._id, iss: "EMH" },
+      process.env.JWT_SECRET
+    );
 
-        // email data
-        const emailData = {
-            from: "noreply@node-react.com",
-            to: email,
-            subject: "Password Reset Instructions",
-            text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/reset-password/${token}`,
-            html: `<p>Please use the following link to reset your password:</p> <p>${process.env.CLIENT_URL}/reset-password/${token}</p>`,
-        };
+    // email data
+    const emailData = {
+      from: "noreply@node-react.com",
+      to: email,
+      subject: "Password Reset Instructions",
+      text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/reset-password/${token}`,
+      html: `<p>Please use the following link to reset your password:</p> <p>${process.env.CLIENT_URL}/reset-password/${token}</p>`,
+    };
 
-        return user.updateOne({ resetPasswordLink: token }, (err, success) => {
-            if (err) {
-                return res.json({ message: err });
-            } else {
-                sendEmail(emailData);
-                return res.status(200).json({
-                    message: `Email has been sent to ${email}. Follow the instructions to reset your password.`,
-                });
-            }
+    return user.updateOne({ resetPasswordLink: token }, (err, success) => {
+      if (err) {
+        return res.json({ message: err });
+      } else {
+        sendEmail(emailData);
+        return res.status(200).json({
+          message: `Email has been sent to ${email}. Follow the instructions to reset your password.`,
         });
+      }
     });
+  });
 };
 
 exports.resetPassword = (req, res) => {
-    const { resetPasswordLink, newPassword } = req.body;
+  const { resetPasswordLink, newPassword } = req.body;
 
-    User.findOne({ resetPasswordLink }, (err, user) => {
-        if (err || !user)
-            return res.status("401").json({
-                error: "Invalid Link!",
-            });
+  User.findOne({ resetPasswordLink }, (err, user) => {
+    if (err || !user)
+      return res.status("401").json({
+        error: "Invalid Link!",
+      });
 
-        const updatedFields = {
-            password: newPassword,
-            resetPasswordLink: "",
-        };
+    const updatedFields = {
+      password: newPassword,
+      resetPasswordLink: "",
+    };
 
-        user = _.extend(user, updatedFields);
-        // user.updated = Date.now();
-        user.save((err, result) => {
-            if (err) {
-                return res.status(400).json({
-                    error: err,
-                });
-            }
-            res.json({
-                message: `Great! Now you can login with your new password.`,
-            });
+    user = _.extend(user, updatedFields);
+    // user.updated = Date.now();
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
         });
+      }
+      res.json({
+        message: `Great! Now you can login with your new password.`,
+      });
     });
+  });
 };
 
 exports.signout = (req, res) => {
-    res.clearCookie("t");
-    res.json({ message: "Signout success" });
+  res.clearCookie("t");
+  res.json({ message: "Signout success" });
 };
