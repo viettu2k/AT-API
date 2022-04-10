e97210c86cc366052d7f5d85a454039724439463
viettu2k <doanviettu2k@gmail.com>
2022-04-10 22:57:30 +0700

    change password

--- controllers/user.js
+++ controllers/user.js
@@ -4,88 +4,96 @@ const _ = require("lodash");
 const fs = require("fs");
 
 exports.userById = (req, res, next, id) => {
-    User.findById(id).exec((err, user) => {
-        if (err || !user) {
-            return res.status(400).json({ error: "User not found" });
-        }
-        req.profile = user;
-        next();
-    });
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+    req.profile = user;
+    next();
+  });
 };
 
 exports.read = (req, res) => {
-    req.profile.hashed_password = undefined;
-    req.profile.salt = undefined;
-    req.profile.photo = undefined;
-    return res.json(req.profile);
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  req.profile.photo = undefined;
+  return res.json(req.profile);
 };
 
 exports.update = (req, res) => {
-    let form = new formidable.IncomingForm();
-    // console.log("incoming form data: ", form);
-    form.keepExtensions = true;
-    form.parse(req, (err, fields, files) => {
-        console.log(err);
-        if (err) {
-            return res.status(400).json({
-                error: "Photo could not be uploaded",
-            });
-        }
-        // save user
-        let user = req.profile;
-        user = _.extend(user, fields);
+  let form = new formidable.IncomingForm();
+  // console.log("incoming form data: ", form);
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    console.log(err);
+    if (err) {
+      return res.status(400).json({
+        error: "Photo could not be uploaded",
+      });
+    }
+    // save user
+    let user = req.profile;
+    user = _.extend(user, fields);
 
-        if (files.photo) {
-            user.photo.data = fs.readFileSync(files.photo.path);
-            user.photo.contentType = files.photo.type;
-        }
+    if (files.photo) {
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+    }
 
-        user.save((err, result) => {
-            if (err) {
-                return res.status(400).json({
-                    error: err,
-                });
-            }
-            result.hashed_password = undefined;
-            result.salt = undefined;
-            result.photo = undefined;
-            res.json(result);
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
         });
+      }
+      result.hashed_password = undefined;
+      result.salt = undefined;
+      result.photo = undefined;
+      res.json(result);
     });
+  });
 };
 
 exports.changePassword = (req, res) => {
-    const { id, newPassword } = req.body;
+  const { password, newPassword } = req.body;
 
-    User.findOne({ id }, (err, user) => {
-        if (err || !user)
-            return res.status("401").json({
-                error: "Invalid id!",
-            });
+  const { _id } = req.profile;
 
-        const updatedFields = {
-            password: newPassword,
-        };
+  User.findOne({ _id }, (err, user) => {
+    if (err || !user)
+      return res.status("401").json({
+        error: "Invalid id!",
+      });
 
-        user = _.extend(user, updatedFields);
-        // user.updated = Date.now();
-        user.save((err, result) => {
-            if (err) {
-                return res.status(400).json({
-                    error: err,
-                });
-            }
-            res.json({
-                message: `Great! Now you can login with your new password.`,
-            });
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Password dont match",
+      });
+    }
+
+    const updatedFields = {
+      password: newPassword,
+    };
+
+    user = _.extend(user, updatedFields);
+    // user.updated = Date.now();
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
         });
+      }
+      res.json({
+        message: `Great! Now you can login with your new password.`,
+      });
     });
+  });
 };
 
 exports.userPhoto = (req, res, next) => {
-    if (req.profile.photo.data) {
-        res.set(("Content-Type", req.profile.photo.contentType));
-        return res.send(req.profile.photo.data);
-    }
-    next();
+  if (req.profile.photo.data) {
+    res.set(("Content-Type", req.profile.photo.contentType));
+    return res.send(req.profile.photo.data);
+  }
+  next();
 };
--- routes/user.js
+++ routes/user.js
@@ -4,16 +4,21 @@ const router = express.Router();
 const { requireSignin, isAuth } = require("../controllers/auth");
 
 const {
-    userById,
-    read,
-    update,
-    changePassword,
-    userPhoto,
+  userById,
+  read,
+  update,
+  changePassword,
+  userPhoto,
 } = require("../controllers/user");
 
 router.get("/users/:userId", requireSignin, isAuth, read);
 router.put("/users/:userId", requireSignin, isAuth, update);
-router.put("users/change-password", requireSignin, isAuth, changePassword);
+router.put(
+  "users/:userId/change-password",
+  requireSignin,
+  isAuth,
+  changePassword
+);
 router.get("/users/photo/:userId", userPhoto);
 
 router.param("userId", userById);
