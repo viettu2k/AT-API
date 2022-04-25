a86425c81d5cfc326759dba1a1ac46c44a0ae96d
viettu2k <doanviettu2k@gmail.com>
2022-04-25 09:11:01 +0700

    fix

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -2,7 +2,6 @@ const Attendance = require("../models/attendance");
 const Student = require("../models/student");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 const _ = require("lodash");
-const fs = require("fs");
 
 exports.attendanceById = (req, res, next, id) => {
   Attendance.findById(id).exec((err, classroom) => {
@@ -37,32 +36,5 @@ exports.create = async (req, res) => {
       });
     }
     res.json({ message: "Created successfully", data });
-  });
-};
-
-exports.update = (req, res) => {
-  let classroom = req.classroom;
-  classroom = _.extend(classroom, req.body);
-  classroom.save((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err),
-      });
-    }
-    res.json(data);
-  });
-};
-
-exports.remove = (req, res) => {
-  const classroom = req.classroom;
-  classroom.remove((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err),
-      });
-    }
-    res.json({
-      message: "Classroom deleted",
-    });
   });
 };
