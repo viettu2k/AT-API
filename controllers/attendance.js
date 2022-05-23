75c17a7ef35bf1c6b81e6f59c8dde89e38d2da6f
viettu2k <doanviettu2k@gmail.com>
2022-05-23 15:05:29 +0700

    improve

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -8,152 +8,156 @@ puppeteer.use(StealthPlugin());
 require('dotenv').config();
 
 exports.attendanceById = (req, res, next, id) => {
-    Attendance.findById(id).exec((err, attendance) => {
-        if (err || !attendance) {
-            return res.status(400).json({
-                error: 'Attendance does not exist',
-            });
-        }
-        req.attendance = attendance;
-        next();
-    });
+  Attendance.findById(id).exec((err, attendance) => {
+    if (err || !attendance) {
+      return res.status(400).json({
+        error: 'Attendance does not exist',
+      });
+    }
+    req.attendance = attendance;
+    next();
+  });
 };
 
 exports.read = (req, res) => {
-    return res.json(req.attendance);
+  return res.json(req.attendance);
 };
 
-exports.create = async(req, res) => {
-    const { classId } = req.body;
-    const participants = await Student.find({ classId }).select('-photo');
+exports.create = async (req, res) => {
+  const { classId } = req.body;
+  const participants = await Student.find({ classId }).select('-photo');
 
-    let attendance = new Attendance({
-        ...req.body,
-    });
+  let attendance = new Attendance({
+    ...req.body,
+  });
 
-    attendance = _.extend(attendance, { participants });
+  attendance = _.extend(attendance, { participants });
 
-    attendance.save((err, data) => {
-        if (err) {
-            return res.status(400).json({
-                error: err,
-            });
-        }
-        res.json({ message: 'Created successfully', data });
-    });
+  attendance.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({ message: 'Created successfully', data });
+  });
 };
 
 exports.update = (req, res) => {
-    let attendance = req.attendance;
-    attendance = _.extend(attendance, req.body);
-    attendance.save((err, data) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err),
-            });
-        }
-        res.json(data);
-    });
+  let attendance = req.attendance;
+  attendance = _.extend(attendance, req.body);
+  attendance.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
 };
 
-exports.automaticallyAttendance = async(req, res) => {
-    try {
-        let attendance = req.attendance;
-        const { meetLink } = req.body;
-        const googleLogin = 'https://accounts.google.com/signin';
-        const browser = await puppeteer.launch({
-            headless: false,
-            args: [
-                '--use-fake-ui-for-media-stream',
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-            ],
-        });
-        const page = await browser.newPage();
-        await page.goto(googleLogin);
-        await page.waitForSelector('input[type="email"]');
-        await page.type('input[type="email"]', process.env.GOOGLE_ID);
-        await Promise.all([
-            page.waitForNavigation(),
-            await page.keyboard.press('Enter'),
-        ]);
-        await page.waitForSelector('input[type="password"]', { visible: true });
-        await page.type('input[type="password"]', process.env.GOOGLE_PASSWORD);
-        await Promise.all([
-            await page.keyboard.press('Enter'),
-            page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        ]);
-        await page.goto(meetLink);
-        await page.waitForTimeout(1000);
-        await page.keyboard.down('ControlLeft');
-        await page.keyboard.press('KeyD');
-        await page.keyboard.press('KeyE');
-        await page.keyboard.up('ControlLeft');
-        await page.evaluate(() => {
-            document
-                .querySelector(
-                    '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span'
-                )
-                .click();
-        });
-        await page.waitForSelector('.XEazBc.adnwBd');
-        const grabStudentNames = await page.evaluate(async() => {
-            const nameTags = document.querySelectorAll('.XEazBc.adnwBd');
-            let studentNames = [];
-            nameTags.forEach((tag) => {
-                studentNames.push(tag.innerText);
-            });
-            return studentNames;
-        });
-        const result = grabStudentNames;
-        await browser.close();
+exports.automaticallyAttendance = async (req, res) => {
+  try {
+    let attendance = req.attendance;
+    const { meetLink } = req.body;
+    const googleLogin = 'https://accounts.google.com/signin';
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: [
+        '--use-fake-ui-for-media-stream',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+    });
+    const page = await browser.newPage();
+    await page.goto(googleLogin);
+    await page.waitForSelector('input[type="email"]');
+    await page.type('input[type="email"]', process.env.GOOGLE_ID);
+    await Promise.all([
+      page.waitForNavigation(),
+      await page.keyboard.press('Enter'),
+    ]);
+    await page.waitForSelector('input[type="password"]', { visible: true });
+    await page.type('input[type="password"]', process.env.GOOGLE_PASSWORD);
+    await Promise.all([
+      await page.keyboard.press('Enter'),
+      page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ]);
+    await page.goto(meetLink);
+    await page.waitForSelector(
+      '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span',
+      { visible: true }
+    );
+    // await page.waitForTimeout(1000);
+    await page.keyboard.down('ControlLeft');
+    await page.keyboard.press('KeyD');
+    await page.keyboard.press('KeyE');
+    await page.keyboard.up('ControlLeft');
+    await page.evaluate(() => {
+      document
+        .querySelector(
+          '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span'
+        )
+        .click();
+    });
+    await page.waitForSelector('.XEazBc.adnwBd', { visible: true });
+    const grabStudentNames = await page.evaluate(async () => {
+      const nameTags = document.querySelectorAll('.XEazBc.adnwBd');
+      let studentNames = [];
+      nameTags.forEach((tag) => {
+        studentNames.push(tag.innerText);
+      });
+      return studentNames;
+    });
+    const result = grabStudentNames;
+    console.log(result);
+    await browser.close();
 
-        for (let i = 0; i < attendance.participants.length; i++) {
-            for (let j = 0; j < result.length; j++) {
-                const id = attendance.participants[i].studentId.slice(-4);
-                if (result[j].includes(id)) {
-                    attendance.participants[i].status = true;
-                }
-            }
+    for (let i = 0; i < attendance.participants.length; i++) {
+      for (let j = 0; j < result.length; j++) {
+        const id = attendance.participants[i].studentId.slice(-4);
+        if (result[j].includes(id)) {
+          attendance.participants[i].status = true;
         }
-
-        attendance.save((err, data) => {
-            if (err) {
-                return res.status(400).json({
-                    error: errorHandler(err),
-                });
-            }
-            console.log(data);
-            res.json(data);
-        });
-    } catch (error) {
-        // console.log(error);
-        return res.status(500).send('Something went wrong. Please try again');
+      }
     }
+
+    attendance.save((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(data);
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(500).send('Something went wrong. Please try again');
+  }
 };
 
 exports.listAttendanceByClass = (req, res) => {
-    Attendance.find({ classId: req.classroom._id })
-        .populate('classId', '_id')
-        .sort('-createdAt')
-        .exec((err, attendances) => {
-            if (err) {
-                return res.status(400).json({ error: err });
-            }
-            res.json(attendances);
-        });
+  Attendance.find({ classId: req.classroom._id })
+    .populate('classId', '_id')
+    .sort('-createdAt')
+    .exec((err, attendances) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      res.json(attendances);
+    });
 };
 
 exports.remove = (req, res) => {
-    let attendance = req.attendance;
-    attendance.remove((err, deletedAttendance) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err),
-            });
-        }
-        res.json({
-            message: 'Check Attendance deleted successfully',
-        });
+  let attendance = req.attendance;
+  attendance.remove((err, deletedAttendance) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: 'Check Attendance deleted successfully',
     });
+  });
 };
--- index.js
+++ index.js
@@ -6,117 +6,117 @@ const https = require('https');
 const fs = require('fs');
 puppeteer.use(StealthPlugin());
 
-const download = (url, destination) =>
-    new Promise((resolve, reject) => {
-        const file = fs.createWriteStream(destination);
+// const download = (url, destination) =>
+//     new Promise((resolve, reject) => {
+//         const file = fs.createWriteStream(destination);
 
-        https
-            .get(url, (response) => {
-                response.pipe(file);
+//         https
+//             .get(url, (response) => {
+//                 response.pipe(file);
 
-                file.on('finish', () => {
-                    file.close(resolve(true));
-                });
-            })
-            .on('error', (error) => {
-                fs.unlink(destination);
+//                 file.on('finish', () => {
+//                     file.close(resolve(true));
+//                 });
+//             })
+//             .on('error', (error) => {
+//                 fs.unlink(destination);
 
-                reject(error.message);
-            });
-    });
+//                 reject(error.message);
+//             });
+//     });
 
 async function run(googleMeet) {
-    const browser = await puppeteer.launch({
-        headless: false,
-        // executablePath: 'C://Program Files//Google//Chrome//Application//chrome.exe',
-        args: [
-            '--use-fake-ui-for-media-stream',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            // '--user-data-dir=%d0azvjettu%\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1',
-            // '--profile-directory=Profile 1',
-        ],
+  const browser = await puppeteer.launch({
+    headless: false,
+    // executablePath: 'C://Program Files//Google//Chrome//Application//chrome.exe',
+    args: [
+      '--use-fake-ui-for-media-stream',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      // '--user-data-dir=%d0azvjettu%\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1',
+      // '--profile-directory=Profile 1',
+    ],
+  });
+
+  const page = await browser.newPage();
+  await page.goto(googleLogin);
+  // // await page.waitFor(1000);
+  await page.waitForSelector('input[type="email"]');
+  await page.type('input[type="email"]', 'doazvjettu0012');
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.keyboard.press('Enter'),
+  ]);
+  await page.waitForSelector('input[type="password"]', { visible: true });
+  await page.type('input[type="password"]', 'd0azvjettu');
+  await Promise.all([
+    await page.keyboard.press('Enter'),
+    page.waitForNavigation({ waitUntil: 'networkidle2' }),
+  ]);
+  await page.goto(googleMeet);
+
+  // await page.waitForSelector(
+  //     '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span'
+  // );
+  await page.waitForTimeout(1000);
+  await page.evaluate(() => {
+    document
+      .querySelector(
+        '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span'
+      )
+      .click();
+  });
+
+  await page.waitForSelector('.XEazBc.adnwBd', { visible: true });
+  const grabStudentNames = await page.evaluate(async () => {
+    const nameTags = document.querySelectorAll('.XEazBc.adnwBd');
+    let studentNames = [];
+    nameTags.forEach((tag) => {
+      studentNames.push(tag.innerText);
     });
+    return studentNames;
+  });
 
-    const page = await browser.newPage();
-    await page.goto(googleLogin);
-    // // await page.waitFor(1000);
-    await page.waitForSelector('input[type="email"]');
-    await page.type('input[type="email"]', 'doazvjettu0012');
-    await Promise.all([
-        page.waitForNavigation(),
-        await page.keyboard.press('Enter'),
-    ]);
-    await page.waitForSelector('input[type="password"]', { visible: true });
-    await page.type('input[type="password"]', 'd0azvjettu');
-    await Promise.all([
-        await page.keyboard.press('Enter'),
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-    ]);
-    await page.goto(googleMeet);
+  //  const grabStudentImages = await page.evaluate(async () => {
+  //    const nameTags = document.querySelectorAll('.XEazBc.adnwBd');
+  //    let studentNames = [];
+  //    nameTags.forEach((tag) => {
+  //      studentNames.push(tag.innerText);
+  //    });
 
-    // await page.waitForSelector(
-    //     '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span'
-    // );
-    await page.waitForTimeout(1000);
-    await page.evaluate(() => {
-        document
-            .querySelector(
-                '#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > span'
-            )
-            .click();
-    });
+  //  });
 
-    await page.waitForSelector('.XEazBc.adnwBd');
-    const grabStudentNames = await page.evaluate(async() => {
-        const nameTags = document.querySelectorAll('.XEazBc.adnwBd');
-        let studentNames = [];
-        nameTags.forEach((tag) => {
-            studentNames.push(tag.innerText);
-        });
-        return studentNames;
-    });
+  // await page.waitForSelector(
+  //     '#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div:nth-child(2) > div.zWfAib.eFmLfc.nrxduf.a1pVef > div:nth-child(2) > div.koV58.Zi94Db > div.LBDzPb > div'
+  // ); // Method to ensure that the element is loaded
+  // const logo = await page.$(
+  //     '#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div:nth-child(2) > div.zWfAib.eFmLfc.nrxduf.a1pVef > div:nth-child(2) > div.koV58.Zi94Db > div.LBDzPb > div'
+  // ); // logo is the element you want to capture
+  // await logo.screenshot({
+  //     path: 'test.png',
+  // });
 
-    //  const grabStudentImages = await page.evaluate(async () => {
-    //    const nameTags = document.querySelectorAll('.XEazBc.adnwBd');
-    //    let studentNames = [];
-    //    nameTags.forEach((tag) => {
-    //      studentNames.push(tag.innerText);
-    //    });
+  //   const images = await page.evaluate(() =>
+  //     Array.from(document.images, (e) => e.src)
+  //   );
 
-    //  });
+  //   for (let i = 0; i < images.length; i++) {
+  //     result = await download(images[i], `${grabStudentNames[i]}.png`);
 
-    // await page.waitForSelector(
-    //     '#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div:nth-child(2) > div.zWfAib.eFmLfc.nrxduf.a1pVef > div:nth-child(2) > div.koV58.Zi94Db > div.LBDzPb > div'
-    // ); // Method to ensure that the element is loaded
-    // const logo = await page.$(
-    //     '#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div:nth-child(2) > div.zWfAib.eFmLfc.nrxduf.a1pVef > div:nth-child(2) > div.koV58.Zi94Db > div.LBDzPb > div'
-    // ); // logo is the element you want to capture
-    // await logo.screenshot({
-    //     path: 'test.png',
-    // });
+  //     if (result === true) {
+  //       console.log('Success:', images[i], 'has been downloaded successfully.');
+  //     } else {
+  //       console.log('Error:', images[i], 'was not downloaded.');
+  //       console.error(result);
+  //     }
+  //   }
 
-    const images = await page.evaluate(() =>
-        Array.from(document.images, (e) => e.src)
-    );
+  console.log(grabStudentNames);
 
-    for (let i = 0; i < images.length; i++) {
-        result = await download(images[i], `${grabStudentNames[i]}.png`);
+  await browser.close();
 
-        if (result === true) {
-            console.log('Success:', images[i], 'has been downloaded successfully.');
-        } else {
-            console.log('Error:', images[i], 'was not downloaded.');
-            console.error(result);
-        }
-    }
-
-    console.log(grabStudentNames);
-
-    await browser.close();
-
-    // page.on("dialog", async (dialog) => {
-    //   await dialog.dismiss();
-    // });
+  // page.on("dialog", async (dialog) => {
+  //   await dialog.dismiss();
+  // });
 }
 run('https://meet.google.com/wgu-kjxc-mfo?authuser=1');
